16935fedcdd3c3d3ba9be99e2a4087832fd35fd0
Shawn Patel <shawnpatel@berkeley.edu>
2021-05-18 10:21:49 -0700

    initial commit

---
+++ app/main/logger/logger.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void info_log(char *message);
+
+void warn_log(char *message);
+
+void err_log(char *message);
---
+++ app/main/main.c
@@ -0,0 +1,10 @@
+/*
+    Radgard
+*/
+
+#include <stdio.h>
+#include "logger/logger.h"
+
+void app_main(void) {
+    info_log("Test.");
+}
